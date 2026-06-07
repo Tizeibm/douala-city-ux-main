1ec93f3830216889fc6d79101ea3b26f6e99127e
Tize Ibrahim Ahmed <tizeahmed750@gmail.com>
2026-06-07 22:46:14 +0100

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://doualacity-api.onrender.com/api' // Replace with your actual Render deployment URL
+  apiUrl: 'http://localhost:8080/api' // Replace with your actual Render deployment URL
 };
